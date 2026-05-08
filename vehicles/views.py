d63b642e032ef88e1f3342d7826d6f4ef999de11
naomisanchez-coder <naomi.sanchez@tecsup.edu.pe>
2026-05-08 14:49:47 -0500

    add ViewSet for Owner and Vehicle with search filter

--- vehicles/views.py
+++ vehicles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Owner, Vehicle
+from .serializers import OwnerSerializer, VehicleSerializer
 
-# Create your views here.
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'license_number']
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['brand', 'model']
