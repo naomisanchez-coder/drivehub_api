4ca9a9dc8f4042360bea1471148eb49e53b0cecc
naomisanchez-coder <naomi.sanchez@tecsup.edu.pe>
2026-05-08 14:47:03 -0500

    add Owner and Vehicle models

--- vehicles/models.py
+++ vehicles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    name = models.CharField(max_length=100)
+    license_number = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Vehicle(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='vehicles')
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
